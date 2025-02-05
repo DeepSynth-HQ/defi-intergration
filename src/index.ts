1a68749010a8c520074a225ee825a96967a1fe92
phuquiivo03 <52100996@student.tdtu.edu.vn>
2025-02-05 15:46:43 +0700

    update swap endpoint

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(
 );
 
 app.use(express.json({ limit: "10mb" }));
-app.post("/cetusSwap", async (_req, res) => {
+app.post("/swap", async (_req, res) => {
   const body: ICetusSwap = _req.body;
   const result = await cetusSwap(body);
   res.send(result);
